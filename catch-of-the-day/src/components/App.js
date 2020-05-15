89833f1426b601b76568b064712d5bf924a17181
Theo Voss <theo.voss973@gmail.com>
2020-05-14 22:50:18 -0400

    First 13 videos
    Functional up through create store, and add a fish to the inventory

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+
+class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        const fishes = {...this.state.fishes};
+        fishes[`fish${Date.now}`] = fish;
+        this.setState({fishes});
+    };
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market"/>
+                </div>
+                <Inventory addFish={this.addFish}/>
+                <Order />
+            </div>
+        )
+    }
+}
+
+export default App;
